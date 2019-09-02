1059a0fa79451c6f5a233fc952f6dbcd90667a5e
sh3at90 <Mohamed@admins-MacBook-Pro.local>
2019-09-02 13:12:26 +0200

    test jenkins

--- News-MVVM/ViewController/LoginViewController.swift
+++ News-MVVM/ViewController/LoginViewController.swift
@@ -30,7 +30,7 @@ class LoginViewController: UIViewController {
     }
     
     // MARK: - Functions
-    func setupTextFields(){
+    func setupTextFields() {
         userNameTextField.textFiled.returnKeyType = .next
         passwordTextField.textFiled.returnKeyType = .done
         passwordTextField.textFiled.text = ""
@@ -94,7 +94,7 @@ extension LoginViewController: ControllerType {
     }
     
     static func create(with viewModel: LoginViewModel) -> UIViewController {
-        let storyboard = UIStoryboard(name: "Authorization", bundle: nil)
+      //  let storyboard = UIStoryboard(name: "Authorization", bundle: nil)
         let controller = storyboard.instantiateViewController(withIdentifier: "LoginViewController") as! LoginViewController
         controller.viewModel = viewModel
         return controller
