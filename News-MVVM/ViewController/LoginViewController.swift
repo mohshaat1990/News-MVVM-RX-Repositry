4dba461e6bca5e4b479770233da7a83ce3e8d7b7
sh3at90 <Mohamed@admins-MacBook-Pro.local>
2019-09-02 15:26:39 +0200

    continous integration

--- News-MVVM/ViewController/LoginViewController.swift
+++ News-MVVM/ViewController/LoginViewController.swift
@@ -94,6 +94,9 @@ extension LoginViewController: ControllerType {
     }
     
     static func create(with viewModel: LoginViewModel) -> UIViewController {
+        
+        
+        
        let storyboard = UIStoryboard(name: "Authorization", bundle: nil)
         let controller = storyboard.instantiateViewController(withIdentifier: "LoginViewController") as! LoginViewController
         controller.viewModel = viewModel
